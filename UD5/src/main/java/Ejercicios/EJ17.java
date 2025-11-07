55e412ae0272c2f7a38d3b12e3a693aa238a6cfa
jorpasmon <jorpasmon@alu.edu.gva.es>
2025-11-07 09:33:25 +0100

    Programacion

--- UD5/src/main/java/Ejercicios/EJ17.java
+++ UD5/src/main/java/Ejercicios/EJ17.java
@@ -8,13 +8,42 @@ public class EJ17 {
         String hora;
         System.out.print("Indique la hora en formato HH:MM:SS: ");
         hora = sc.nextLine();
-        char h1  = hora.charAt(0);
+        //Horas
+        char h1 = hora.charAt(0);
         char h2 = hora.charAt(1);
-        char h3 = hora.charAt(2);
-        char h4 = hora.charAt(3);
-        char h5 = hora.charAt(4);
-        char h6 = hora.charAt(5);
-        char h7 = hora.charAt(6);
-        char h8 = hora.charAt(7);
+        //Minutos
+        char m1 = hora.charAt(3);
+        char m2 = hora.charAt(4);
+        //Segundos
+        char s1 = hora.charAt(6);
+        char s2 = hora.charAt(7);
+        String hora1 = String.valueOf(h1 + h2);
+        String min1 = String.valueOf(m1 + m2);
+        String seg1 = String.valueOf(s1 + s2);
+        int horas = Integer.parseInt(hora1);
+        int minutos = Integer.parseInt(min1);
+        int segundos = Integer.parseInt(seg1);
+        segundos = segundos + 1;
+        if (segundos < 59) {
+            System.out.println(horas + ":" + minutos + ":" + segundos);
+        }
+        else {
+            segundos = 0;
+            minutos = minutos +1;
+            if  (minutos < 59) {
+                System.out.println(horas + ":" + minutos + ":" + segundos);
+            }
+            else {
+                minutos = 0;
+                horas = horas +1;
+                if (horas < 23) {
+                    System.out.println(horas + ":" + minutos + ":" + segundos);
+                }
+                else {
+                    horas = 0;
+                    System.out.println(horas + ":" + minutos + ":" + segundos);
+                }
+            }
+        }
     }
 }
