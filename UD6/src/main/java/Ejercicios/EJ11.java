7003389c056beb6031b575d147635af2460f8499
jorpasmon <jorpasmon@alu.edu.gva.es>
2025-11-27 13:47:41 +0100

    Programacion

--- UD6/src/main/java/Ejercicios/EJ11.java
+++ UD6/src/main/java/Ejercicios/EJ11.java
@@ -1,11 +1,9 @@
 package Ejercicios;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class EJ11 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         final int FIN = 100;
         int numeros1[];
         numeros1 = new int[FIN];
@@ -15,7 +13,7 @@ public class EJ11 {
             numeros1[i - 1] = i;
         }
         for (int i = 1; i <= FIN; i++) {
-            
+            numeros2[i - 1] = numeros1.length - i + 1;
         }
         System.out.println(Arrays.toString(numeros1));
         System.out.println(Arrays.toString(numeros2));
---
+++ UD6/src/main/java/Ejercicios/EJ12.java
@@ -0,0 +1,41 @@
+package Ejercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EJ12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String opcion;
+        final int FIN = 10;
+        int numeros[];
+        int p;
+        int v;
+        numeros = new int[FIN];
+        do {
+            System.out.println("a. Mostrar valores\n" +
+                    "b. Introducir valores\n" +
+                    "c. Salir\n");
+            System.out.print("Selecciona una opcion: ");
+            opcion = sc.nextLine();
+            if (opcion.equalsIgnoreCase("a")) {
+                System.out.println(Arrays.toString(numeros));
+                System.out.println();
+            }
+            else if (opcion.equalsIgnoreCase("b")) {
+                System.out.print("Indique el número que quiere introducir en el array: ");
+                v =  sc.nextInt();
+                do {
+                    System.out.print("Indique la posicion donde quiere introducir el número: ");
+                    p = sc.nextInt();
+                    if (p < 0 || p > FIN) {
+                        System.out.println("Error: posicion invalida");
+                    }
+                } while (p < 0 || p > FIN);
+                    numeros[p] = v;
+                System.out.println();
+            }
+        } while (opcion.equalsIgnoreCase("A") || opcion.equalsIgnoreCase("B"));
+        System.out.println("Saliendo del programa.");
+    }
+}
